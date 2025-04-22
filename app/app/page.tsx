2a97c295d00df4d043a2d770e707d67c3ff4a171
dselokela023 <dselokela023@student.wethinkcode.co.za>
2025-04-23 01:41:47 +0200

    successfully transferred the from vite to next

---
+++ app/app/page.tsx
@@ -0,0 +1,47 @@
+
+"use client"
+import { Toaster } from "./components/ui/toaster";
+import { Toaster as Sonner } from "./components/ui/sonner";
+import { TooltipProvider } from "./components/ui/tooltip";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Web3Provider } from "./context/Web3Context";
+import Navbar from "./components/layout/Navbar";
+import Footer from "./components/layout/Footer";
+import Index from "./pages/Index";
+import VvipAccess from "./pages/VvipAccess";
+import Courses from "./pages/Courses";
+import Socials from "./pages/Socials";
+import Contact from "./pages/Contact";
+import NotFound from "./pages/NotFound";
+
+const queryClient = new QueryClient();
+
+const Home = () => (
+  <QueryClientProvider client={queryClient}>
+    <Web3Provider>
+      <TooltipProvider>
+        <Toaster />
+        <Sonner />
+        <BrowserRouter>
+          <div className="flex flex-col min-h-screen">
+            <Navbar />
+            <main className="flex-grow">
+              <Routes>
+                <Route path="/" element={<Index />} />
+                <Route path="/vvip" element={<VvipAccess />} />
+                <Route path="/courses" element={<Courses />} />
+                <Route path="/socials" element={<Socials />} />
+                <Route path="/contact" element={<Contact />} />
+                <Route path="*" element={<NotFound />} />
+              </Routes>
+            </main>
+            <Footer />
+          </div>
+        </BrowserRouter>
+      </TooltipProvider>
+    </Web3Provider>
+  </QueryClientProvider>
+);
+
+export default Home;
